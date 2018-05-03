dbeff4e11476570501a7857962d748e420edf079
kk-taqi <rizki.nix@gmail.com>
2018-05-03 15:23:46 +0000

    new manual docker installation

---
+++ install_docker_new.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#based on https://docs.docker.com/
+
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+ 
+ 
+sudo apt-get update
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+sudo usermod -aG docker vagrant
